45594706861a7ceb953284917e288539b23c969a
Dr. Neil Brittliff <thesourorange@outlook.com>
2025-06-06 10:47:58 +1000

    Update DirectoryExporter.java

--- src/main/java/au/org/tso/ldap/navigator/DirectoryExporter.java
+++ src/main/java/au/org/tso/ldap/navigator/DirectoryExporter.java
@@ -1,15 +1,23 @@
 package au.org.tso.ldap.navigator;
 
-import org.apache.directory.api.ldap.model.cursor.EntryCursor;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.directory.api.ldap.model.entry.Entry;
 import org.apache.directory.api.ldap.model.entry.Attribute;
-import org.apache.directory.api.ldap.model.message.SearchScope;
+import org.apache.directory.api.ldap.model.schema.AttributeType;
 import org.apache.directory.ldap.client.api.LdapConnection;
 import org.bouncycastle.asn1.*;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DirectoryExporter {
+
+    @Autowired
+    SchemaExplorer schemaExplorer;
 
     String bytesToHex(byte[] bytes) {
         StringBuilder sb = new StringBuilder();
@@ -21,42 +29,57 @@ public class DirectoryExporter {
 
     }
 
-    public byte[] export(LdapConnection connection, String searchBase) throws Exception {
+    boolean isHumanReadable(String value) {
+        Pattern pattern = Pattern.compile("[^\\p{ASCII}]", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(value);
+        boolean matchFound = matcher.find();
 
-        try (EntryCursor cursor = connection.search(searchBase, "(objectclass=*)", SearchScope.OBJECT)) {
+        return (!matchFound);
 
-            for (Entry entry : cursor) {
-                ASN1EncodableVector attributeVector = new ASN1EncodableVector();
+    }
 
-                for (Attribute attribute : entry.getAttributes()) {
-                    String attributeName = attribute.getAttributeType().getName();
-                    ASN1ObjectIdentifier oid = new ASN1ObjectIdentifier(attribute.getAttributeType().getOid());
+    public byte[] export(LdapConnection connection, String dn) throws Exception {
+        var logger = LoggerFactory.getLogger(DirectoryExplorer.class);
 
-                    if (attribute.isHumanReadable()) {
-                        attributeVector.add(new DERSequence(new ASN1Encodable[] {
-                                new DERUTF8String(attributeName), oid, new DERUTF8String(attribute.getString())
-                        }));
+        Map<String, AttributeType> schemaAttributes = schemaExplorer.load(connection);
 
-                    } else {
-                        attributeVector.add(new DERSequence(new ASN1Encodable[] {
-                                new DERUTF8String(attributeName), oid, new DEROctetString(attribute.getBytes())
-                        }));
-                    }
+        Entry entry = connection.lookup(dn);
 
+        if (entry == null) {
+            logger.info("Object: '" + dn + "' not Found);
+
+            throw new Exception("Object: '" + dn + "' not Found");
+        }
+
+        ASN1EncodableVector attributeVector = new ASN1EncodableVector();
+
+        for (Attribute attribute : entry.getAttributes()) {
+
+            if (schemaAttributes.containsKey(attribute.getId())) {
+                String attributeName = attribute.getUpId();
+
+                ASN1ObjectIdentifier oid = new ASN1ObjectIdentifier(schemaAttributes.get(attribute.getId()).getOid());
+
+                if (attribute.isHumanReadable()) {
+                    attributeVector.add(new DERSequence(new ASN1Encodable[] {
+                            new DERUTF8String(attributeName), oid, new DERUTF8String(attribute.getString())
+                    }));
+
+                } else {
+                    attributeVector.add(new DERSequence(new ASN1Encodable[] {
+                            new DERUTF8String(attributeName), oid, new DEROctetString(attribute.getBytes())
+                    }));
                 }
-
-                ASN1Sequence attributeSequence = new DERSequence(attributeVector);
-
-                // Output the ASN.1 encoded data
-                attributeSequence.getEncoded();
-
-                return attributeSequence.getEncoded();
 
             }
 
-            throw new Exception("DN - '" + searchBase + "' - not found");
-
         }
+
+        ASN1Sequence attributeSequence = new DERSequence(attributeVector);
+
+        logger.info("Export '" + dn + "' - successful");
+        
+        return attributeSequence.getEncoded();
 
     }
 
