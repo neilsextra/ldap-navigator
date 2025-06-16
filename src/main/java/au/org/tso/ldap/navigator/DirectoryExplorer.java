668e9a34106ce981dfe8fa79cf118af6122d2757
Dr. Neil Brittliff <thesourorange@outlook.com>
2025-06-16 14:46:54 +1000

    Update DirectoryExplorer.java

--- src/main/java/au/org/tso/ldap/navigator/DirectoryExplorer.java
+++ src/main/java/au/org/tso/ldap/navigator/DirectoryExplorer.java
@@ -1,14 +1,26 @@
-package au.org.tso.ldap.navigator;
+package au.gov.sa.euc.ldap.navigator;
+
 import java.util.Map;
 import java.util.Vector;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.Base64;
 import java.util.HashMap;
+import java.util.List;
 
 import org.apache.directory.api.ldap.model.cursor.EntryCursor;
+import org.apache.directory.api.ldap.model.cursor.SearchCursor;
 import org.apache.directory.api.ldap.model.entry.Entry;
 import org.apache.directory.api.ldap.model.entry.Attribute;
+import org.apache.directory.api.ldap.model.message.Control;
+import org.apache.directory.api.ldap.model.message.ResultCodeEnum;
+import org.apache.directory.api.ldap.model.message.SearchRequest;
+import org.apache.directory.api.ldap.model.message.SearchRequestImpl;
 import org.apache.directory.api.ldap.model.message.SearchScope;
+import org.apache.directory.api.ldap.model.message.controls.PagedResults;
+import org.apache.directory.api.ldap.model.message.controls.PagedResultsImpl;
+import org.apache.directory.api.ldap.model.name.Dn;
 import org.apache.directory.api.ldap.model.schema.AttributeType;
 import org.apache.directory.ldap.client.api.LdapConnection;
 import org.slf4j.LoggerFactory;
@@ -18,7 +30,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class DirectoryExplorer {
 
-    final int MAX_RESULTS = 1000;
+    interface ResultContainer {
+
+        List<String> getResults();
+
+        String getDn();
+
+        String getCursorPosition();
+    };
+
+    final int MAX_RESULTS = 100;
 
     @Autowired
     SchemaExplorer schemaExplorer;
@@ -45,48 +66,124 @@ public class DirectoryExplorer {
 
     }
 
-    Vector<String> search(LdapConnection connection, String dn, int start) throws Exception {
-        Vector<String> entries = new Vector<String>();
+    SearchResponse search(LdapConnection connection, final String dn) throws Exception {
+        var logger = LoggerFactory.getLogger(DirectoryExplorer.class);
 
-        int startPosition = 0;
-        int row = 0;
+        List<String> entries = new ArrayList<>();
+        final StringBuffer cursorPosition = new StringBuffer();
+        var pageSize = MAX_RESULTS;
 
-        try (EntryCursor cursor = connection.search(dn, "(objectclass=*)", SearchScope.OBJECT)) {
+        logger.info("Primary Search...");
 
-            if (start == 0) {
+        try (EntryCursor cursor = connection.search(dn, "(objectclass=*)",
+                SearchScope.OBJECT)) {
 
-                for (Entry entry : cursor) {
-
-                    entries.add(entry.getDn().toString());
-
-                }
-
-                row = 1;
-            } 
-            
-        }
-
-        try (EntryCursor cursor = connection.search(dn, "(objectclass=*)", SearchScope.ONELEVEL)) {
-
-            loop: for (Entry entry : cursor) {
-
-                if (startPosition < start) {
-                    startPosition += 1;
-                    continue loop;
-                }
-
-                if (row >= MAX_RESULTS) {
-                    break loop;
-                }
+            for (Entry entry : cursor) {
 
                 entries.add(entry.getDn().toString());
-                row += 1;
+                pageSize = pageSize - 1;
 
             }
 
+            cursor.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
         }
 
-        return entries;
+       PagedResults pagedControl = new PagedResultsImpl();
+
+        pagedControl.setSize(pageSize);
+
+        SearchRequest searchRequest = new SearchRequestImpl();
+        searchRequest.setBase(new Dn(dn));
+        searchRequest.setTimeLimit(10000);
+        searchRequest.setFilter("(objectclass=*)");
+        searchRequest.setScope(SearchScope.ONELEVEL);
+        searchRequest.addControl(pagedControl);
+
+        try (SearchCursor cursor = connection.search(searchRequest)) {
+   
+            while (cursor.next()) {
+                Entry entry = cursor.getEntry();
+
+                entries.add(entry.getDn().toString());
+
+            }
+
+            logger.info("Capturing Cursor position");
+
+            if (cursor.getSearchResultDone().getLdapResult().getResultCode() != ResultCodeEnum.SIZE_LIMIT_EXCEEDED) {
+
+                Map<String, Control> controls = cursor.getSearchResultDone().getControls();
+                PagedResults responseControl = (PagedResults) controls.get(PagedResults.OID);
+
+                if (responseControl != null) {
+                    cursorPosition.append(Base64.getEncoder().encodeToString(responseControl.getCookie()));    
+                }
+
+            }
+
+            cursor.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+        logger.info("Search Completed - Returning Results...");
+
+        return new SearchResponse(entries, dn, cursorPosition.toString());
+
+    }
+
+    SearchResponse next(LdapConnection connection, String dn, String cursorPosition) throws Exception {
+        var logger = LoggerFactory.getLogger(DirectoryExplorer.class);
+
+        final List<String> entries = new ArrayList<>();
+        final StringBuffer nextCursorPosition = new StringBuffer();
+
+        PagedResultsImpl pageControl = new PagedResultsImpl();
+        pageControl.setSize(MAX_RESULTS);
+        pageControl.setCookie(Base64.getDecoder().decode(cursorPosition));
+
+        SearchRequestImpl searchRequest = new SearchRequestImpl();
+        searchRequest.setBase(new Dn(dn));
+        searchRequest.setFilter("(objectclass=*)");
+        searchRequest.setScope(SearchScope.SUBTREE);
+        searchRequest.addControl(pageControl);
+    
+        try (SearchCursor cursor = connection.search(searchRequest)) {
+            System.out.println("Cursor returned: " + cursor.isDone());
+            while (cursor.next()) {
+                Entry entry = cursor.getEntry();
+                entries.add(entry.getDn().toString());
+
+                System.out.println(entry.getDn().toString());
+            }
+
+            if (cursor.getSearchResultDone().getLdapResult().getResultCode() != ResultCodeEnum.SIZE_LIMIT_EXCEEDED) {
+
+                Map<String, Control> controls = cursor.getSearchResultDone().getControls();
+
+                PagedResults responseControl = (PagedResults) controls.get(PagedResults.OID);
+
+                if (responseControl != null) {
+                    nextCursorPosition.append(Base64.getEncoder().encodeToString(responseControl.getCookie()));
+                    logger.info("Next: Captured Cursor position: " +  new String(responseControl.getCookie()) + " : " + nextCursorPosition.toString());
+                }
+
+            }
+
+            cursor.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+        return new SearchResponse(entries, dn, nextCursorPosition.toString());
 
     }
 
