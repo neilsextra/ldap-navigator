6d638c6de6191db3cd42284b518510b7f1513d4b
Dr. Neil Brittliff <thesourorange@outlook.com>
2026-06-29 15:54:01 +1000

    Update AttributeUtils.java

--- src/main/java/au/org/tso/ldap/navigator/util/AttributeUtils.java
+++ src/main/java/au/org/tso/ldap/navigator/util/AttributeUtils.java
@@ -3,8 +3,19 @@ package au.org.tso.ldap.navigator.util;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * AttributeUtils
+ * 
+ * Helper functions to decode directory attributes
+ */
+
 final public class AttributeUtils {
     
+    /**
+     * Convert a binary value to hex
+     * @param bytes the binary to convert 
+     * @return a hexadecimal representation of the binary value
+     */
     public static String bytesToHex(byte[] bytes) {
         StringBuilder sb = new StringBuilder();
         for (byte b : bytes) {
@@ -15,6 +26,11 @@ final public class AttributeUtils {
 
     }
 
+    /**
+     * Determine if an attribute value is human readable (not binary)
+     * @param value the value
+     * @return 'true' human readable, 'false' otherwise
+     */
     public static boolean isHumanReadable(String value) {
         Pattern pattern = Pattern.compile("[^\\p{ASCII}]", Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(value);
