a60b634c9e1e83f20262a0c0df68c5727bf7252b
Dr. Neil Brittliff <thesourorange@outlook.com>
2026-06-30 15:06:56 +1000

    Update SearchResponse.java

--- src/main/java/au/org/tso/ldap/navigator/SearchResponse.java
+++ src/main/java/au/org/tso/ldap/navigator/SearchResponse.java
@@ -2,27 +2,61 @@ package au.org.tso.ldap.navigator;
 
 import java.util.Set;
 
+import org.apache.directory.api.ldap.model.message.SearchScope;
+
+/**
+ * Search Response
+ * 
+ * container of a list of distinguished names as a result of a search
+ * 
+ */
 class SearchResponse implements DirectoryExplorer.ResultContainer {
     Set<String> results;
-    String dn;
-    
-    SearchResponse(Set<String> results, String dn) {
+    String base;
+    String filter;
+    SearchScope scope;
+
+    /**
+     * Search response constructor
+     * @param results the distinguised names
+     * @param base the 
+     * @param filter
+     * @param scope
+     */
+    SearchResponse(Set<String> results, String base, String filter, SearchScope scope) {
         this.results = results;
-        this.dn = dn;
+        this.base = base;
+        this.filter = filter;
+        this.scope = scope;
+
     }
 
-	@Override
-	public Set<String> getResults() {
+    @Override
+    public Set<String> getResults() {
 
         return results;
 
-	}
+    }
 
-	@Override
-	public String getDn() {
+    @Override
+    public String getBase() {
 
-        return dn;
+        return base;
 
     }
-    
+
+    @Override
+    public String getFilter() {
+
+        return filter;
+
+    }
+
+    @Override
+    public SearchScope getScope() {
+
+        return scope;
+
+    }
+
 }
